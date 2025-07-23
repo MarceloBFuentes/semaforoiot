370d60a7f7f0f651cb99b5b5531f4757e7a34735
Marcelo Bruno Fuentes <marcelo.bruno.fuentes@hotmail.com>
2025-07-23 10:06:24 -0300

    teste

---
+++ script.js
@@ -0,0 +1,19 @@
+function ligar_vermelho() {
+    alert("ligando led Vermelho")
+}
+
+function ligar_amarelo() {
+    alert("ligando led Amarelo")
+}
+
+function ligar_verde() {
+    alert("ligando led Verde")
+}
+
+function ligar_automatico() {
+    alert("ligando Automatico")
+}
+
+function desligar() {
+    alert("Desligar")
+}
