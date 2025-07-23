ec56c128c4c448b4634b6d4ad5dc8c103be4d6ee
Marcelo Bruno Fuentes <marcelo.bruno.fuentes@hotmail.com>
2025-07-23 11:57:29 -0300

    ew

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let clientWeb = null
 
 const clientId = "Esp32" + Math.floor(Math.random() * 900) + 100
-clientWeb = new Paho.MQTT.Client("broker.hivemq.com", 8884, clientId)
+clientWeb = new Paho.MQTT.Client("broker.hivemq.com", 1883, clientId)
 
 clientWeb.connect({
-    useSSL: true,
+    useSSL: false,
     timeout: 5,
     onSuccess: function(){
         alert("conectado com sucesso")
@@ -18,16 +18,25 @@ clientWeb.connect({
 function ligar_vermelho() {
     //alert("ligando led Vermelho")
     document.getElementById("vermelho").classList.add("vermelho")
+    const msg = new Paho.MQTT.Message("")
+    msg.destinationName = "senai/led/ligar_vermelho"
+    clientWeb.send(msg)
 }
 
 function ligar_amarelo() {
     //alert("ligando led Amarelo")
     document.getElementById("amarelo").classList.add("amarelo")
+    const msg = new Paho.MQTT.Message("")
+    msg.destinationName = "senai/led/ligar_amarelo"
+    clientWeb.send(msg)
 }
 
 function ligar_verde() {
    // alert("ligando led Verde")
     document.getElementById("verde").classList.add("verde")
+    const msg = new Paho.MQTT.Message("")
+    msg.destinationName = "senai/led/ligar_verde"
+    clientWeb.send(msg)
 }
 
 function ligar_automatico() {
@@ -40,4 +49,7 @@ function desligar() {
     document.getElementById("vermelho").classList.remove("vermelho")
     document.getElementById("amarelo").classList.remove("amarelo")
     document.getElementById("verde").classList.remove("verde")
+    const msg = new Paho.MQTT.Message("")
+    msg.destinationName = "senai/led/desligartudo"
+    clientWeb.send(msg)
 }
